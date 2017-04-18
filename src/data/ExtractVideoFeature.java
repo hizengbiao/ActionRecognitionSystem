b320bc004a830fa140e6510055bb457773c86713
hizengbiao <hizengbiao@qq.com>
2017-04-18 16:38:47 +0800

    little change

--- src/data/ExtractVideoFeature.java
+++ src/data/ExtractVideoFeature.java
@@ -34,11 +34,11 @@ public class ExtractVideoFeature {
 //	int frame_width;
 //	int frame_height;
 //	double frameCount;
-	static double xuandu_max = 50;// 旋度最大值，若为正，则最小为50
-	static double xuandu_min = -50;// 旋度最小值
+	static double xuandu_max = 40;// 旋度最大值，若为正，则最小为50
+	static double xuandu_min = -40;// 旋度最小值
 	static double xuandu_fazhi = 0.2;// 旋度阀值
 	static int scale = 60;// 矩形框尺寸
-	static int featurePointNumberBorder = 5;// 特征点数量的阀值
+	static int featurePointNumberBorder = 3;// 特征点数量的阀值
 
 	public ExtractVideoFeature() {
 		
--- src/data/Start.java
+++ src/data/Start.java
@@ -11,16 +11,16 @@ public class Start {
 	public static void main(String args[]) throws IOException {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 		
-		new MainWindow().lanuchWindow();
+//		new MainWindow().lanuchWindow();
 		
-/*		//提取所有视频特征：
+		//提取所有视频特征：
 		ExtractAllVideos extA=new ExtractAllVideos();
 		extA.exe();
 		
 //		训练：
 		MySVM.loadTrainData();
 //		MySVM.saveTrainDataTest();
-		MySVM.train();*/
+		MySVM.train();
 		
 /*//		预测：
 		Labels c=Labels.HANDWAVING;
