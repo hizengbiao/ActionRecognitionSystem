2631147b073a427b5ec7fa80a17654c1be287383
hizengbiao <hizengbiao@qq.com>
2017-05-31 10:40:17 +0800

    ignore

--- src/data/ExtractVideoFeature.java
+++ src/data/ExtractVideoFeature.java
@@ -31,10 +31,10 @@ public class ExtractVideoFeature {
 	static double xuandu_max_tem = 200;// 旋度最大值，若为正，则最小为50
 //	static double xuandu_min = -40;// 旋度最小值
 	static double xuandu_fazhi = 0.2;// 旋度阀值
-	static int scale = 60;// 矩形框尺寸
+	static int scale = 80;// 矩形框尺寸
 	static int featurePointNumberBorder = 3;// 特征点数量的阀值
 	public static int speed = 0;// 播放视频时各帧的间隔
-	static int spaceTimeSize=5;
+	static int spaceTimeSize=10;
 
 	public ExtractVideoFeature() {
 
--- src/data/MyConstants.java
+++ src/data/MyConstants.java
@@ -32,7 +32,7 @@ public class MyConstants {
     public static String S_videoCapture="videoCapture";
     public static String S_Console="Console";
     
-    public static int TrainVideoCount=2;//训练的视频样本数
+    public static int TrainVideoCount=30;//训练的视频样本数
     public static int ExtractVideoCount=4;
     public static int K=7;//KNN的K值
     public static int KNN_threshold=0;//距离为K的范围内，要有KNN_threshold个以上的特征才算数
